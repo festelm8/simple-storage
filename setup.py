174f93eac0f76d4bc5fa0e8617d3057e31956f9a
Artem Kanev <a-kanev@yandex.ru>
2017-05-16 15:00:15 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""
+Simple Storage
+-----
+Hash file storage.
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask',
+    version='0',
+    url='https://github.com/nixorn/simple-storage/',
+    packages=['simple_storage'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+    ],
+    classifiers=[
+        'Development Status :: Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
